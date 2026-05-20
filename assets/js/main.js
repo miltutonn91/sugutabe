660b119e882a6733650c232c25b19df1e0bfb8cd
梅田実沙 <miltu@MacBook-Pro-2.local>
2026-05-21 01:20:43 +0900

    Remove unused images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,15 @@
 'use strict';
 
-    $(function () {
-        // ハンバーガークリックで開閉
-        $('.hamburger-icon').on('click', function () {
-          $(this).toggleClass('close');     // ✖ に変化
-          $('.nav').toggleClass('open');    // nav 表示・非表示
-        });
-    
-        // メニュー内リンククリックで閉じる
-        $('.nav__item a').on('click', function () {
-          $('.hamburger-icon').removeClass('close');  // ✖→🍔 に戻す
-          $('.nav').removeClass('open');              // nav 閉じる
-        });
-      });
+$(function () {
+  // ハンバーガークリックで開閉
+  $('.hamburger-icon').on('click', function () {
+    $(this).toggleClass('close'); // ✖ に変化
+    $('.nav').toggleClass('open'); // nav 表示・非表示
+  });
 
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const heart = document.getElementById('heart');
-//   if (heart) {
-//     heart.addEventListener('click', function () {
-//       this.classList.toggle('liked');
-//     });
-//   }
-// });
-
-
-
-
-
+  // メニュー内リンククリックで閉じる
+  $('.nav__item a').on('click', function () {
+    $('.hamburger-icon').removeClass('close'); // ✖→🍔 に戻す
+    $('.nav').removeClass('open'); // nav 閉じる
+  });
+});
